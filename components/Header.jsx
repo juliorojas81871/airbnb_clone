3dbb5f57220e807abf814358b19872d53b2f25c6
Julio Rojas <juliorojas81871@gmail.com>
2022-12-06 21:42:26 -0500

    first commit

---
+++ components/Header.jsx
@@ -0,0 +1,56 @@
+import Image from "next/image";
+import React, { useState } from "react";
+import {
+  MagnifyingGlassIcon,
+  GlobeAltIcon,
+  Bars3Icon,
+  UserCircleIcon,
+  UsersIcon,
+} from "@heroicons/react/24/solid";
+
+const Header = () => {
+
+    return (
+    <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-3 md:px-10">
+      {/* left - Logo */}
+      <div
+        onClick={() => router.push("/")}
+        className="relative flex items-center h-10 cursor-pointer my-auto"
+      >
+        <Image
+          src="/airbnb_logo.png"
+          layout="fill"
+          objectFit="contain"
+          objectPosition="left"
+        />
+      </div>
+
+      {/* middle - Search */}
+      <div className="flex items-center min-w-[230px] md:border-2 rounded-full py-2 md:shadow-sm">
+        <input
+          value={()=>{}}
+          onChange={()=>{}}
+          className="hidden md:inline flex-grow pl-5 bg-transparent outline-none text-sm text-gray-600 placeholder-gray-400"
+          type="text"
+        />
+        <MagnifyingGlassIcon
+          onClick={()=>{}}
+          className="hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer md:mx-2"
+        />
+      </div>
+
+      {/* right - Become a host + Icons */}
+      <div className="flex space-x-4 items-center justify-end text-gray-500">
+        <p className="hidden lg:inline cursor-pointer">Become a host</p>
+        <GlobeAltIcon className="h-6 cursor-pointer" />
+        <div className="flex items-center space-x-2 border-2 p-2 rounded-full cursor-pointer">
+          <Bars3Icon className="h-6" />
+          <UserCircleIcon className="h-6" />
+        </div>
+      </div>
+      
+    </header>
+  );
+}
+
+export default Header;
